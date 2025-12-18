ae376ccb83fa39feef01d9c76c3af2e31be531e2
Sarp Akgündüz <sakgunduz@proven.com.tr>
2025-12-18 15:00:28 +0300

    client message sending feature is removed from client and added to utils since both server and client will send messages. But these sections will still modify the method for their purpose.

--- TCP/TcpClient.java
+++ TCP/TcpClient.java
@@ -3,19 +3,5 @@ import java.io.*;
 import java.net.*;
 
 public class TcpClient {
-    public void sendMessage(String host, int port, byte[] message) throws Exception {
-        Socket socket = new Socket(host, port);
-        OutputStream output = socket.getOutputStream();
-        
-        output.write(message);
-        
-        // Print sent message in hex format
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : message) {
-            hexString.append(String.format("%02X ", b));
-        }
-        System.out.println("Message is sent: " + hexString.toString().trim());
-        
-        socket.close();
-    }
+    
 }
---
+++ TCP/TcpUtils.java
@@ -0,0 +1,24 @@
+package TCP;
+import java.io.*;
+import java.net.*;
+
+public class TcpUtils {
+    /**
+     * Sends a byte array message to specified host and port
+     */
+    public void sendMessage(String host, int port, byte[] message) throws Exception {
+        Socket socket = new Socket(host, port);
+        OutputStream output = socket.getOutputStream();
+        
+        output.write(message);
+        
+        // Print sent message in hex format
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : message) {
+            hexString.append(String.format("%02X ", b));
+        }
+        System.out.println("Message is sent: " + hexString.toString().trim());
+        
+        socket.close();
+    }
+}
