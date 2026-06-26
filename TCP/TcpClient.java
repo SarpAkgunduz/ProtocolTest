42c047ca24d350f52b72fa912e48d11ad02ca0f2
Sarp Akgündüz <sakgunduz@proven.com.tr>
2026-06-26 12:51:51 +0300

    client util division

--- TCP/TcpClient.java
+++ TCP/TcpClient.java
@@ -1,7 +1,23 @@
 package TCP;
-import java.io.*;
-import java.net.*;
 
 public class TcpClient {
-    
+
+    private final String host;
+    private final int port;
+    private final TcpUtils utils;
+
+    public TcpClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+        this.utils = new TcpUtils();
+    }
+
+    public void send(byte[] message) throws Exception {
+        utils.sendMessage(host, port, message);
+    }
+
+    public static void main(String[] args) throws Exception {
+        TcpClient client = new TcpClient("127.0.0.1", 8888);
+        client.send(new byte[]{ 0x24 });
+    }
 }
